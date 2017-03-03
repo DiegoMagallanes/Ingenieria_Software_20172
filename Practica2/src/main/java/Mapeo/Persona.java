5e6714d2c8b6648b0c53ef759a3d33b4e34db24d
diego <diego@dalmos>
2017-03-02 22:50:08 -0600

    practica1.2 - Parte 2 (Modificada)

--- Practica2/src/main/java/Mapeo/Persona.java
+++ Practica2/src/main/java/Mapeo/Persona.java
@@ -5,6 +5,8 @@
  */
 package Mapeo;
 
+import static java.util.Calendar.DATE;
+import java.util.Date;
 import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -13,7 +15,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
-
+import javax.persistence.Temporal;
+import static javax.persistence.TemporalType.DATE;
 
 /**
  *
@@ -22,49 +25,52 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "persona")
 public class Persona {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "idpersona")
     private int id_persona;
-    
+
     @Column(name = "nombre")
     private String nombre;
-    
+
     @Column(name = "fechanac")
-    private String fecha_Nac;
-    
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date fecha_Nac;
+
     @Column(name = "carrera")
     private String carrera;
-    
-    public int get_Id_Persona(){
+
+    public int getId_persona() {
         return id_persona;
     }
-    
-    public String get_Nombre(){
-        return nombre;
-    }
-    
-    public String get_FechaNac(){
-        return fecha_Nac;
-    }
-    
-    public String get_Carrera(){
-        return carrera;
-    }
-    
-    public void set_Id_Persona(int id_persona){
+
+    public void setId_persona(int id_persona) {
         this.id_persona = id_persona;
     }
-    
-    public void set_Nombre(String nombre){
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-    
-    public void set_Fecha_Nac(String fecha_nac){
-        this.fecha_Nac = fecha_nac;
+
+    public Date getFecha_Nac() {
+        return fecha_Nac;
     }
-    
-    public void set_Carrera(String carrera){
+
+    public void setFecha_Nac(Date fecha_Nac) {
+        this.fecha_Nac = fecha_Nac;
+    }
+
+    public String getCarrera() {
+        return carrera;
+    }
+
+    public void setCarrera(String carrera) {
         this.carrera = carrera;
     }
+
 }
