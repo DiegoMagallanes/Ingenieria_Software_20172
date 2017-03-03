2fe2b8fa2ddf878277be7b04fabb5e20911b8bd5
diego <diego@dalmos>
2017-03-02 22:37:04 -0600

    practica1.2 - Parte 2 (Modificada)

--- Practica2/src/main/java/Mapeo/User.java
+++ Practica2/src/main/java/Mapeo/User.java
@@ -22,51 +22,54 @@ import javax.persistence.Table;
  */
 @Entity
 @Table(name = "user")
-public class User{
+public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "iduser")
     private int id_user;
-    
+
     @Column(name = "correo")
     private String correo;
-    
+
     @Column(name = "password")
     private String password;
-    
+
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "idpersona")
     private Persona persona_id;
-    
-    public int get_Id_User(){
+
+    public int getId_user() {
         return id_user;
     }
-    
-    public String get_Correo(){
-        return correo;
-    }
-    
-    public String get_Password(){
-        return password;
-    }
-    
-    public Persona get_Persona_Id(){
-        return persona_id;
-    }
-    
-    public void set_Id_User(int id_user){
+
+    public void setId_user(int id_user) {
         this.id_user = id_user;
     }
-    
-    public void set_Correo(String correo){
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
         this.correo = correo;
     }
-    
-    public void set_Password(String password){
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
         this.password = password;
     }
-    
-    public void set_Persona_Id(Persona persona_id){
+
+    public Persona getPersona_id() {
+        return persona_id;
+    }
+
+    public void setPersona_id(Persona persona_id) {
         this.persona_id = persona_id;
     }
+
+    
 }
